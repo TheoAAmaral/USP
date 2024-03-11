0a6fee4555fd65ea900731018b492bbaf25590f6
TheoAAmaral <160811825+TheoAAmaral@users.noreply.github.com>
2024-03-11 17:24:03 +0000

    -m

---
+++ c_teste/teste.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Inclui a biblioteca padrão de entrada e saída
+
+int main() // Função principal
+{
+    printf("Hello, World!\n"); // Imprime a mensagem "Hello, World!" na tela
+    return 0; // Retorna 0 para o sistema operacional
+}
