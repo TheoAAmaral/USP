aa7bd176780ba9cf4486b993a4ba76b0031706a0
TheoAAmaral <160811825+TheoAAmaral@users.noreply.github.com>
2024-08-19 21:59:15 +0000

    No teste.h for Aula04

--- PCS3111/Aula04/teste.h
+++ PCS3111/Aula04/teste.h
@@ -1,7 +1,33 @@
-#ifndef TESTE_H
-#define TESTE_H
+#include <iostream>
+#include <string>
 #include "Hotel.h"
 
-void teste1();
-void teste2();
-#endif
+using namespace std;
+
+void teste1() {
+    Reserva *quarto31 = new Reserva;
+    quarto31->setQuarto(31);
+    quarto31->setInicio(1);
+    quarto31->setFim(6);
+
+    quarto31->imprimir();
+}
+
+void teste2() {
+    Reserva *quarto22 = new Reserva;
+    Reserva *quarto25 = new Reserva;
+
+    quarto22->setFim(8);
+    quarto22->setInicio(1);
+    quarto22->setQuarto(22);
+
+    quarto25->setFim(9);
+    quarto25->setInicio(5);
+    quarto25->setQuarto(25);
+
+    Hotel *hotel = new Hotel;
+
+    hotel->adicionar(quarto22);
+    hotel->adicionar(quarto25);
+    hotel->imprimir();  
+}
